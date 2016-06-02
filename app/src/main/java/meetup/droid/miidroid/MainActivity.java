d0fa02f893a4476cf12094e1a292b0c5dc177568
Naveed <Naveed.Khan2@capitalone.com>
2016-06-01 20:52:55 -0400

    Lesson 3

--- app/src/main/java/meetup/droid/miidroid/MainActivity.java
+++ app/src/main/java/meetup/droid/miidroid/MainActivity.java
@@ -1,19 +1,27 @@
 package meetup.droid.miidroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String EXPECTED_USERNAME = "userOne";
+
+    private static final String EXPECTED_PASSWORD = "password";
 
     private EditText etUsername;
 
     private EditText etPassword;
 
     private Button btnLogin;
+
+    private TextView tvErrorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         etUsername = (EditText) findViewById(R.id.et_username);
         etPassword = (EditText) findViewById(R.id.et_password);
         btnLogin = (Button) findViewById(R.id.btn_login);
+        tvErrorMessage = (TextView) findViewById(R.id.tv_error);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +46,13 @@ public class MainActivity extends AppCompatActivity {
         String username = etUsername.getText().toString();
         String password = etPassword.getText().toString();
 
-        Log.d("TAG", "Username is: " + username + "\n" +
-            "Password is: " + password);
+        if (username.equals(EXPECTED_USERNAME) && password.equals(EXPECTED_PASSWORD)) {
+            tvErrorMessage.setVisibility(View.GONE);
+            //Log user in
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        } else {
+            tvErrorMessage.setVisibility(View.VISIBLE);
+        }
     }
 }
